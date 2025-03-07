7255c51a22235b2833a56bda0ca399e04f22f7c1
Su_Yee_Myat_Moe <suyeemyatmoe21@gmail.com>
2025-03-08 01:46:40 +0630

    Dependency injection in position controller

--- HRMS Web/Controllers/PositionController.cs
+++ HRMS Web/Controllers/PositionController.cs
@@ -1,10 +1,16 @@
-﻿using HRMS_Web.Models.ViewModels;
+﻿using HRMS_Web.DAO;
+using HRMS_Web.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HRMS_Web.Controllers
 {
     public class PositionController : Controller
     {
+        public readonly HRMSWebDBContext _dbcontext;//Dependency injection dbContext
+        public PositionController(HRMSWebDBContext dbContext)
+        {
+            _dbcontext = dbContext;
+        }
         [HttpGet]
         public IActionResult Entry()
         {
@@ -13,6 +19,7 @@ namespace HRMS_Web.Controllers
         [HttpPost]
         public IActionResult Entry(PositionViewModel model)
         {
+            //DTO (data transfer object) [to change data from viewmodel to datamodel]
             return View();
         }
     }
