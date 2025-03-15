187eb7a6b9916f60682621f7e9dac7dd2d0545bb
Su_Yee_Myat_Moe <suyeemyatmoe21@gmail.com>
2025-03-15 18:55:01 +0630

    Department CRUD

--- HRMS Web/Controllers/DepartmentController.cs
+++ HRMS Web/Controllers/DepartmentController.cs
@@ -59,6 +59,7 @@ namespace HRMS_Web.Controllers
         public IActionResult List()
         {
             IList<DepartmentViewModel> departmentViews = _dbcontext.Departments
+       .Where(w => w.IsActive)//same with  == true
        .OrderBy(o => o.CreatedAt)
        .Select(s => new DepartmentViewModel
        {
@@ -71,7 +72,80 @@ namespace HRMS_Web.Controllers
 
             return View(departmentViews);// this will return the list of positions (without adding positionViews will return null in Model of view page)
         }
-        
+        public IActionResult Edit(string id)
+        {
+            DepartmentViewModel departmentView = _dbcontext.Departments//similar to list 
+                .Where(w => w.IsActive && w.Id == id)
+                .Select(s => new DepartmentViewModel
+                {
+                    Id = s.Id,
+                    Code = s.Code,
+                    Description = s.Description,
+                    ExtensionPhone= s.ExtensionPhone
+                }).FirstOrDefault();
+            return View(departmentView);
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(DepartmentViewModel departmentVM)
+        {
+            try
+            {
+                DepartmentEntity currentDepartment = _dbcontext.Departments.Where(w => w.IsActive && w.Id == departmentVM.Id).FirstOrDefault();//retrieve selected data
+                if (currentDepartment is not null)
+                {
+                    currentDepartment.Code = departmentVM.Code;
+                    currentDepartment.Description = departmentVM.Description;
+                    currentDepartment.ExtensionPhone = departmentVM.ExtensionPhone;
+                    currentDepartment.UpdatedAt = DateTime.Now;
+                    currentDepartment.UpdatedBy = "admin";
+                    currentDepartment.Ip = await NetworkHelper.GetIPAddress();
+
+                    _dbcontext.Departments.Update(currentDepartment);//update the record in the context
+                    _dbcontext.SaveChanges();//saving the data in database
+                    //Message to show success 
+                    TempData["Msg"] = "This Position is updated successfully!";
+                    //check if error is occured
+                    TempData["IsErrorOccur"] = false;
+                }
+            }
+            catch (Exception e)
+            {
+                TempData["Msg"] = "There are some errors in saving the record!";
+                //check if error is occured
+                TempData["IsErrorOccur"] = true;
+
+            }
+            return RedirectToAction("List");
+        }
+
+        // /position/delete?id=@item.Id
+        public IActionResult Delete(string id)//for primary key
+        {
+            try
+            {
+                DepartmentEntity departmentEntity = _dbcontext.Departments.Where(w => w.Id == id).FirstOrDefault();//retrive the selected record from database
+                if (departmentEntity != null)
+                {
+                    departmentEntity.IsActive = false;
+                    _dbcontext.Departments.Update(departmentEntity);//update the record in the context
+                    _dbcontext.SaveChanges();//saving the data in database
+                    //Message to show success 
+                    TempData["Msg"] = "This Position is deleted successfully!";
+                    //check if error is occured
+                    TempData["IsErrorOccur"] = false;
+                }
+            }
+            catch (Exception)
+            {
+                TempData["Msg"] = "There are some errors in deleting the record!";
+                //check if error is occured
+                TempData["IsErrorOccur"] = true;
+
+            }
+            return RedirectToAction("List");
+        }
 
     }
 }
