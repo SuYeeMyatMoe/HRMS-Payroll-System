fc3240de5b16353a6cbd4dd8c7b0f2bb1f85fbf5
Su_Yee_Myat_Moe <suyeemyatmoe21@gmail.com>
2025-03-21 08:54:37 +0630

    Structuring the further controllers and update the navigation

---
+++ HRMS Web/Controllers/DailyAttendanceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HRMS_Web.Controllers
+{
+    public class DailyAttendanceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HRMS Web/Controllers/ShiftAttendanceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HRMS_Web.Controllers
+{
+    public class ShiftAttendanceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
