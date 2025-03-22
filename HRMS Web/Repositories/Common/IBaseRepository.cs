131ca69149399746fa96d2ea0b8040528e004620
Su_Yee_Myat_Moe <suyeemyatmoe21@gmail.com>
2025-03-22 16:21:12 +0630

    Repository Pattern Implementation

---
+++ HRMS Web/Repositories/Common/IBaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace HRMS_Web.Repositories.Common
+{
+    public interface IBaseRepository<T> where T:class//(generic interface class as T)
+    {
+        //CRUD process will be written in here
+        
+        void Create (T entity);//Create
+        IEnumerable<T> GetAll (Expression <Func<T,bool>> expression);//Retrieve (Expression is for filtering)
+
+        void Update(T entity);//Update
+        void Delete (T entity);//Delete
+        
+    }
+}
