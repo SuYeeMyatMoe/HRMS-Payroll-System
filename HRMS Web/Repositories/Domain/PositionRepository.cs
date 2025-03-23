a69872639fc60c07036b242f8a0b39bb85fefc86
Su_Yee_Myat_Moe <suyeemyatmoe21@gmail.com>
2025-03-23 11:14:35 +0630

    Inheritance BaseRepository and abstract IPositionRespository

---
+++ HRMS Web/Repositories/Domain/PositionRepository.cs
@@ -0,0 +1,16 @@
+﻿using HRMS_Web.DAO;
+using HRMS_Web.Models.DataModels;
+using HRMS_Web.Repositories.Common;
+
+namespace HRMS_Web.Repositories.Domain
+{
+    public class PositionRepository : BaseRepository<PositionEntity>, IPositionRepository
+    {
+        private readonly HRMSWebDBContext dBContext;
+
+        public PositionRepository(HRMSWebDBContext dBContext) : base(dBContext)
+        {
+            this.dBContext = dBContext;
+        }
+    }
+}
