66922d5130a7081ed6cc10c1e5839168ea72b4b0
Su_Yee_Myat_Moe <suyeemyatmoe21@gmail.com>
2025-03-24 12:45:44 +0630

    CRUD service for position interface

---
+++ HRMS Web/Services/IPositionService.cs
@@ -0,0 +1,23 @@
+﻿using HRMS_Web.Models.ViewModels;
+
+namespace HRMS_Web.Services
+{
+    public interface IPositionService
+    {
+        //CRUD process
+
+        //create
+        void Create(PositionViewModel viewModel);//service will work with viewmodel
+
+        //read
+        IEnumerable<PositionViewModel> GetAll();
+        PositionViewModel GetById(int id);
+
+        //update
+        void Update(PositionViewModel viewModel);
+
+        //delete
+        void Delete(PositionViewModel viewModel);
+
+    }
+}
