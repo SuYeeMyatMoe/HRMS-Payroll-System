5ff742280464e3abcfa89657f072b7b12f912f12
Su_Yee_Myat_Moe <suyeemyatmoe21@gmail.com>
2025-03-24 12:28:23 +0630

    Connecting Unit of Work with respositories to make transaction in database

--- HRMS Web/UnitOfWork/IUnitOfWork.cs
+++ HRMS Web/UnitOfWork/IUnitOfWork.cs
@@ -1,9 +1,16 @@
-﻿namespace HRMS_Web.UnitOfWork
+﻿using HRMS_Web.Repositories.Domain;
+
+namespace HRMS_Web.UnitOfWork
 {
     //for rollback and commit in DBMS transaction management
     public interface IUnitOfWork
     {
         void Commit();
         void Rollback();
+
+        //to connect each repository with unit of work to save in dbs
+        IPositionRepository PositionRepository { get; }
+        IDepartmentRepository DepartmentRepository { get; }
+        IEmployeeRepository EmployeeRepositiory { get; }
     }
 }
