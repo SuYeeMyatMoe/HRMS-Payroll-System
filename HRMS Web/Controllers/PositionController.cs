3989800928e5d383486c2a339dde98a86aaffb8e
Su_Yee_Myat_Moe <suyeemyatmoe21@gmail.com>
2025-03-24 13:26:17 +0630

    Create in PositionService

--- HRMS Web/Controllers/PositionController.cs
+++ HRMS Web/Controllers/PositionController.cs
@@ -23,36 +23,7 @@ namespace HRMS_Web.Controllers
         [HttpPost]
         public async Task<IActionResult> Entry(PositionViewModel position_model)
         {
-            try
-            {
-                //DTO (data transfer object) [to change data from viewmodel to datamodel]
-                PositionEntity positionEntity = new PositionEntity()
-                {
-                    Id = Guid.NewGuid().ToString(),//for PK auto increment
-                    Name = position_model.Name,//from form input
-                    Description = position_model.Description,// from form input
-                    Level = position_model.Level,//from form input
-                    CreatedAt = DateTime.Now,
-                    CreatedBy = "admin",
-                    IsActive = true,
-                    Ip = await NetworkHelper.GetIPAddress()
-                };
-                _dbcontext.Positions.Add(positionEntity);//adding the record to the context [Postions is from DBContext]
-                _dbcontext.SaveChanges();//saving the data to database in here
-
-                //Message to show success 
-                TempData["Msg"] = "New Position is saved successfully!";
-                //check if error is occured
-                TempData["IsErrorOccur"] = false;
-            }
-            catch (Exception e)
-            {
-                TempData["Msg"] = "There are some errors in saving the record!";
-                //check if error is occured
-                TempData["IsErrorOccur"] = true;
-
-            }
-            return RedirectToAction("List");
+            
         }
 
         //To show success result
---
+++ HRMS Web/Services/PositionService.cs
@@ -0,0 +1,69 @@
+﻿using HRMS_Web.Models.DataModels;
+using HRMS_Web.Models.ViewModels;
+using HRMS_Web.UnitOfWork;
+using HRMS_Web.Utilities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRMS_Web.Services
+{
+    public class PositionService : IPositionService
+    {
+        private readonly IUnitOfWork _unitofwork;
+
+
+        //CRUD in controller in will be written in here 
+        //and store in dbs with the help of unit of work
+        public PositionService(IUnitOfWork unitofwork)
+        {
+            this._unitofwork = unitofwork;
+        }
+        public async Task Create(PositionViewModel viewModel)
+        {
+            try
+            {
+                //DTO (data transfer object) [to change data from viewmodel to datamodel]
+                PositionEntity positionEntity = new PositionEntity()
+                {
+                    Id = Guid.NewGuid().ToString(),//for PK auto increment
+                    Name = viewModel.Name,//from form input
+                    Description = viewModel.Description,// from form input
+                    Level = viewModel.Level,//from form input
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "admin",
+                    IsActive = true,
+                    Ip = await NetworkHelper.GetIPAddress()
+                };
+                //save with unit of work
+                _unitofwork.PositionRepository.Create(positionEntity);
+                _unitofwork.Commit();//saving the data to database in here
+
+            }
+            catch (Exception e)
+            {
+                _unitofwork.Rollback();
+
+            }
+            //return RedirectToAction("List");
+        }
+
+        public void Delete(PositionViewModel viewModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<PositionViewModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PositionViewModel GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(PositionViewModel viewModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
