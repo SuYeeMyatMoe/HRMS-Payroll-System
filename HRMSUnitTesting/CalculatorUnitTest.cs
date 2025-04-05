28fe2556f61bf9fa15850a596c43975266a9fc63
Su_Yee_Myat_Moe <suyeemyatmoe21@gmail.com>
2025-04-05 19:50:47 +0630

    Unit Testing with Xunit

---
+++ HRMSUnitTesting/CalculatorUnitTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRMSUnitTesting
+{
+    public class CalculatorUnitTest
+    {
+        [Fact]
+        public void ShouldReturnTrueSum3Nubers()
+        {
+            // (1)Arrange
+            Calculator calculator = new Calculator();
+            int a = 1;
+            int b = 2;
+            int c = 3;
+            int expected = 6;
+            // Act
+            int result = calculator.Sum(a, b, c);//from obj
+            // Assert
+            Assert.Equal(expected, result);//Assert is to check the result from Xunit
+        }
+    }
+}
